625ccb688a1be2c9d4ca2298b5c104f21a6e4aa0
Laraib khan <114330724+Laraibkhan13@users.noreply.github.com>
2024-09-10 18:38:50 +0530

    3rd commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express=require('express');
-
 const app=express();
+const cors=require("cors");
+
+app.use(cors())
+app.use(express.json());
+// app.use("/api/v1,");
 
 app.listen(3000,function(){
-    console.log("server started");
+    console.log("server started at port 3000")
 })
